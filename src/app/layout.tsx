4c186806c96908d11102f677b4bbd5ff7b6cce1e
bender <kudin.dima@gmail.com>
2026-06-01 07:48:51 +0000

    Update theme fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,13 +47,9 @@ export const metadata: Metadata = {
 
 
 
-const libreBaskerville = Libre_Baskerville({
-  variable: "--font-libre-baskerville",
-  subsets: ["latin"],
-  weight: ["400", "700"],
-});
-const inter = Inter({
-  variable: "--font-inter",
+
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
@@ -65,7 +61,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
-        <body className={`${libreBaskerville.variable} ${inter.variable} antialiased`}>
+        <body className={`${manrope.variable} antialiased`}>
           <Tag />
           {children}
           <script
