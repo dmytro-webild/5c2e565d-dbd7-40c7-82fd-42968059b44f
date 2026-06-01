08e45f7b70dc9cbab5ed93e0425af5ad7abf8627
bender <kudin.dima@gmail.com>
2026-06-01 07:48:16 +0000

    Update theme fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { Mulish } from "next/font/google";
 import { Open_Sans } from "next/font/google";
 import { Manrope } from "next/font/google";
 import { DM_Sans } from "next/font/google";
+import { Libre_Baskerville } from "next/font/google";
 
 
 
@@ -45,12 +46,14 @@ export const metadata: Metadata = {
 
 
 
-const manrope = Manrope({
-  variable: "--font-manrope",
+
+const libreBaskerville = Libre_Baskerville({
+  variable: "--font-libre-baskerville",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
-const dmSans = DM_Sans({
-  variable: "--font-dm-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -62,7 +65,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <ServiceWrapper>
-        <body className={`${manrope.variable} ${dmSans.variable} antialiased`}>
+        <body className={`${libreBaskerville.variable} ${inter.variable} antialiased`}>
           <Tag />
           {children}
           <script
